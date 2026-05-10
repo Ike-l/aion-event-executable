48947d32009448d674d3c501c8d83e86f1dbe2df
isaac <isaacloff@gmail.com>
2026-05-10 23:33:32 +0100

    renamed user details trait

--- src/event_executable/executable_filter/mod.rs
+++ src/event_executable/executable_filter/mod.rs
@@ -1,11 +1,11 @@
-use aion_processor::prelude::UserDetails;
+use aion_processor::prelude::UserDetailsGetter;
 use aion_program::prelude::{UserId, UserPassword};
 
 use crate::prelude::EXECUTABLE_USER_DETAILS;
 
 pub struct ExecutableFilter;
 
-impl UserDetails for ExecutableFilter {
+impl UserDetailsGetter for ExecutableFilter {
     fn get_user_details() -> Option<(UserId, UserPassword)> {
         Some(EXECUTABLE_USER_DETAILS)
     }
