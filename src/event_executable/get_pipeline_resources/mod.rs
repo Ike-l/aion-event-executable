5765e2ffc69d68668414f98555da845580a433de
isaac <isaacloff@gmail.com>
2026-05-27 15:54:21 +0100

    Feat: get_pipeline_resources

--- src/event_executable/get_pipeline_resources/mod.rs
+++ src/event_executable/get_pipeline_resources/mod.rs
@@ -1,30 +1,24 @@
 use std::sync::Arc;
 
-use aion_ecs::prelude::GetShared;
-use aion_program::prelude::{AccessBuilder, AccessSubmissionError, DerivedResult, FinalisedAccess, Injection, ProgramRegistry, ResolveResourceError};
-use hecs::Entity;
+use aion_ecs::prelude::Query;
+use aion_program::prelude::{ProgramRegistry, ProgramRegistryResolveEitherError};
+use tokio::runtime::Runtime;
 
-use crate::prelude::PipelineResources;
+use crate::prelude::PipelineResource;
 
-#[derive(small_derive_deref::Deref, small_derive_deref::DerefMut)]
-pub struct GetPipelineResources {
-    pipeline_resources: PipelineResources
+pub trait GetPipelineResources {
+    fn get_pipeline_resources(
+        &self,
+        runtime: Option<&Runtime>
+    ) -> Result<Query<'_, &PipelineResource>, ProgramRegistryResolveEitherError>;
 }
 
-impl Injection for GetPipelineResources {
-    type Item<'new> = GetPipelineResources;
-
-    fn claim_manual_access_builders(accesses: Vec<&AccessBuilder>) -> Vec<usize> { GetShared::<PipelineResources>::claim_manual_access_builders(accesses) }
-
-    fn submit_access(prompted_accesses: Vec<AccessBuilder>) -> Result<Vec<FinalisedAccess>, AccessSubmissionError> {
-        GetShared::<PipelineResources>::submit_access(prompted_accesses)
-    }
-
-    fn resolve_access<'new>(entity: Option<Entity>, program_registry: Arc<ProgramRegistry>, derived_results: Vec<DerivedResult<'new>>) -> Result<Self::Item<'new>, ResolveResourceError> {
-        let pipeline_resources = GetShared::<PipelineResources>::resolve_access(entity, program_registry, derived_results)?;
-
-        Ok(GetPipelineResources {
-            pipeline_resources: (*pipeline_resources.get_shared()).clone()
-        })
+impl GetPipelineResources for Arc<ProgramRegistry> {
+    fn get_pipeline_resources(
+        &self,
+        runtime: Option<&Runtime>
+    ) -> Result<Query<'_, &PipelineResource>, ProgramRegistryResolveEitherError>
+    {
+        self.resolve_simple_either::<Query<&PipelineResource>>(runtime)
     }
 }
