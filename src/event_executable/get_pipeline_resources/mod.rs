77be23146febbc319d78530010e0941418df43c0
isaac <isaacloff@gmail.com>
2026-05-27 15:18:41 +0100

    Fix: get_pipeline_resources uses GetShared

--- src/event_executable/get_pipeline_resources/mod.rs
+++ src/event_executable/get_pipeline_resources/mod.rs
@@ -1,6 +1,6 @@
 use std::sync::Arc;
 
-use aion_ecs::prelude::World;
+use aion_ecs::prelude::{GetShared, World};
 use aion_program::prelude::{AccessBuilder, AccessSubmissionError, DerivedResult, FinalisedAccess, Injection, ProgramRegistry, ResolveResourceError, Shared};
 use hecs::Entity;
 
@@ -15,24 +15,17 @@ pub struct GetPipelineResources {
 impl Injection for GetPipelineResources {
     type Item<'new> = GetPipelineResources;
 
-    fn claim_manual_access_builders(_accesses: Vec<&AccessBuilder>) -> Vec<usize> { vec![] }
+    fn claim_manual_access_builders(accesses: Vec<&AccessBuilder>) -> Vec<usize> { GetShared::<PipelineResources>::claim_manual_access_builders(accesses) }
 
     fn submit_access(prompted_accesses: Vec<AccessBuilder>) -> Result<Vec<FinalisedAccess>, AccessSubmissionError> {
-        Shared::<World>::submit_access(prompted_accesses)
+        GetShared::<PipelineResources>::submit_access(prompted_accesses)
     }
 
     fn resolve_access<'new>(entity: Option<Entity>, program_registry: Arc<ProgramRegistry>, derived_results: Vec<DerivedResult<'new>>) -> Result<Self::Item<'new>, ResolveResourceError> {
-        let world = Shared::<World>::resolve_access(entity, program_registry, derived_results)?;
-        
-        let prepared_pipeline_resources = world
-            .prepare_get_shared::<PipelineResources>(
-                entity
-                    .ok_or(ResolveResourceError::Resolving)?
-            ).ok_or(ResolveResourceError::Resolving)?;
+        let pipeline_resources = GetShared::<PipelineResources>::resolve_access(entity, program_registry, derived_results)?;
 
-        let pipeline_resources = prepared_pipeline_resources.get(&world);
         Ok(GetPipelineResources {
-            pipeline_resources: (*pipeline_resources).clone()
+            pipeline_resources: (*pipeline_resources.get_shared()).clone()
         })
     }
 }
